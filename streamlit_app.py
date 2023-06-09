e6069f010bac076e67d882346025a203e929d429
Emily Marie Ahtúnan <86572370+Emmarie-Ahtunan@users.noreply.github.com>
2023-06-09 02:18:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,6 @@ import pandas as pd
 import streamlit as st
 import pandas as pd
 
-data = pd.read_csv('sample.csv')
-st.title('CSV Sample Data Viewer')
-# Display the DataFrame
-st.dataframe(data)
-
 data = pd.read_csv('test.csv')
 st.title('CSV Test Data Viewer')
 
